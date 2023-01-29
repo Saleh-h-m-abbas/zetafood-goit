779e233a1675e0eae7f3f4f10d9c03972167cc56
Saleh hashem Abbas <40650470+Saleh-h-m-abbas@users.noreply.github.com>
2023-01-29 15:30:06 +0200

    upload action folder again

---
+++ src/components/actions/CustomAlert.jsx
@@ -0,0 +1,25 @@
+import { makeStyles } from '@material-ui/core/styles';
+import { Alert } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    top: theme.spacing(10),
+    left: theme.spacing(-1),
+    width: theme.spacing(30),
+    height: theme.spacing(10),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: theme.shape.borderRadius,
+  },
+}));
+
+export default function CustomAlert(props) {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Alert {...props} />
+    </div>
+  );
+}
