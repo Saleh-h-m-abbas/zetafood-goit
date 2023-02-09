496dba089b0884ea0121a109bf5c38c49295e721
Saleh hashem Abbas <40650470+Saleh-h-m-abbas@users.noreply.github.com>
2023-02-09 12:51:02 +0200

    change to prod database

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "zetafood-dev.firebaseapp.com",
-  projectId: "zetafood-dev",
-  storageBucket: "zetafood-dev.appspot.com",
-  messagingSenderId: "504412099207",
-  appId: "1:504412099207:web:b5f48bbfeefe91f5b29e22",
-  measurementId: "G-88BH1CNGLG"
+  authDomain: "zetafood-a0d87.firebaseapp.com",
+  projectId: "zetafood-a0d87",
+  storageBucket: "zetafood-a0d87.appspot.com",
+  messagingSenderId: "290084228455",
+  appId: "1:290084228455:web:bcceed22a43d776a50f3c2",
+  measurementId: "G-9JDTC577KK"
 };
 
 const app = initializeApp(firebaseConfig);
